56cebb7ec7228bf020dd2771750896210e4baa97
domguz84 <d.guzek84@gmail.com>
2023-01-09 21:15:06 +0100

    .

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //   Exercise exercise = new Exercise();
+        //    exercise.getLength("telewizor");
+        //    exercise.getLength("jest juz noc");
+Exercise exercise = new Exercise();
+exercise.getConcat();
+Exercise.getReplace();
+        }
+    }
+
+
+
